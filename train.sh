48fe66fe36706627a0caef8c071264ba3bdf8a58
Kunwoo Park <70247684+kun-woo-park@users.noreply.github.com>
2021-04-01 01:34:01 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -9,7 +9,7 @@ do
                         python3 colision_avoidance_net_idx.py --num_nodes "${i}" "${j}" "${k}" --num_layers 1 1 1 --index 0 &
                         python3 colision_avoidance_net_idx.py --num_nodes "${i}" "${j}" "${k}" --num_layers 1 1 1 --index 1 &
                         python3 colision_avoidance_net_idx.py --num_nodes "${i}" "${j}" "${k}" --num_layers 1 1 1 --index 2 &
-                        python3 colision_avoidance_net_idx.py --num_nodes "${i}" "${j}" "${k}" --num_layers 1 1 1 --index 3
+                        python3 colision_avoidance_net_idx.py --num_nodes "${i}" "${j}" "${k}" --num_layers 1 1 1 --index 3 &
                         python3 colision_avoidance_net_idx.py --num_nodes "${i}" "${j}" "${k}" --num_layers 2 2 2 --index 0 &
                         python3 colision_avoidance_net_idx.py --num_nodes "${i}" "${j}" "${k}" --num_layers 2 2 2 --index 1 &
                         python3 colision_avoidance_net_idx.py --num_nodes "${i}" "${j}" "${k}" --num_layers 2 2 2 --index 2 &

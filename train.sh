022271a8d52fbaa53b0c40fe3d1ea79b96631b5f
Kunwoo Park <70247684+kun-woo-park@users.noreply.github.com>
2021-04-01 01:35:22 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -6,14 +6,14 @@ do
                 for k in 20 40 60 80
                 do
                         echo "Start train node ${i}_${j}_${k}"
-                        python3 colision_avoidance_net_idx.py --num_nodes "${i}" "${j}" "${k}" --num_layers 1 1 1 --index 0 &
-                        python3 colision_avoidance_net_idx.py --num_nodes "${i}" "${j}" "${k}" --num_layers 1 1 1 --index 1 &
-                        python3 colision_avoidance_net_idx.py --num_nodes "${i}" "${j}" "${k}" --num_layers 1 1 1 --index 2 &
-                        python3 colision_avoidance_net_idx.py --num_nodes "${i}" "${j}" "${k}" --num_layers 1 1 1 --index 3 &
-                        python3 colision_avoidance_net_idx.py --num_nodes "${i}" "${j}" "${k}" --num_layers 2 2 2 --index 0 &
-                        python3 colision_avoidance_net_idx.py --num_nodes "${i}" "${j}" "${k}" --num_layers 2 2 2 --index 1 &
-                        python3 colision_avoidance_net_idx.py --num_nodes "${i}" "${j}" "${k}" --num_layers 2 2 2 --index 2 &
-                        python3 colision_avoidance_net_idx.py --num_nodes "${i}" "${j}" "${k}" --num_layers 2 2 2 --index 3
+                        python3 colision_avoidance_net.py --num_nodes "${i}" "${j}" "${k}" --num_layers 1 1 1 --index 0 &
+                        python3 colision_avoidance_net.py --num_nodes "${i}" "${j}" "${k}" --num_layers 1 1 1 --index 1 &
+                        python3 colision_avoidance_net.py --num_nodes "${i}" "${j}" "${k}" --num_layers 1 1 1 --index 2 &
+                        python3 colision_avoidance_net.py --num_nodes "${i}" "${j}" "${k}" --num_layers 1 1 1 --index 3 &
+                        python3 colision_avoidance_net.py --num_nodes "${i}" "${j}" "${k}" --num_layers 2 2 2 --index 0 &
+                        python3 colision_avoidance_net.py --num_nodes "${i}" "${j}" "${k}" --num_layers 2 2 2 --index 1 &
+                        python3 colision_avoidance_net.py --num_nodes "${i}" "${j}" "${k}" --num_layers 2 2 2 --index 2 &
+                        python3 colision_avoidance_net.py --num_nodes "${i}" "${j}" "${k}" --num_layers 2 2 2 --index 3
                 done
         done
 done

2056b55eabc59731e52f28605264390335af0011
Kunwoo Park <70247684+kun-woo-park@users.noreply.github.com>
2021-04-28 15:41:52 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -5,7 +5,6 @@ do
         do
                 for k in 20 40 60 80
                 do
-                        echo "Start train node ${i}_${j}_${k}"
                         python3 colision_avoidance_net.py --num_nodes "${i}" "${j}" "${k}" --num_layers 1 1 1 --index 0 &
                         python3 colision_avoidance_net.py --num_nodes "${i}" "${j}" "${k}" --num_layers 1 1 1 --index 1 &
                         python3 colision_avoidance_net.py --num_nodes "${i}" "${j}" "${k}" --num_layers 1 1 1 --index 2 &
